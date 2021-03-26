23e9cd8ecb0c30d7e130fae5509a557b6e1bec47
caiolul <caiolucena.il@gmail.com>
2021-03-25 21:58:23 -0300

    🐛 FIX: Adjusting connection to database

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
-from src.views.views import init_app
+import os
+from src.views.views import init_app as views_init
+from src.ext.db.db_connection import configure as configure_db
+from flask_migrate import Migrate
 from flask import Flask
+
+curren_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def create_app():
     app = Flask(__name__)
-    init_app(app)
+    if app.debug == True:
+        path = 'sqlite:////%s/testing/test.db' % (curren_path)
+        app.config['SQLALCHEMY_DATABASE_URI'] = path
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    views_init(app)
+    configure_db(app)
+
+    Migrate(app, app.db)
     return app
--- src/views/views.py
+++ src/views/views.py
@@ -1,7 +1,12 @@
 from flask import Flask
+# from src.ext.db import ...
 
 
 def init_app(app: Flask):
     @app.route('/')
     def index():
         return {'ola': 'mundo'}
+
+    @app.route('/state', methods=['GET', 'POST'])
+    def register_state():
+        return{'patos': ['500 vacinados', '500 sem vacinação']}
