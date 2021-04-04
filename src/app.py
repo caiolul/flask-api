9a8bcb3e83d830dd6ad002006289749435eb845a
caiolul <caiolucena.il@gmail.com>
2021-04-04 17:12:37 -0300

    📦 NEW:  Create routes to State

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from src.ext.views.routes import init_app as views_init
 from src.ext.db.models import configure as configure_db
 from src.ext.api.serializer import configure as config_marshmallow
@@ -6,15 +7,20 @@ from flask_migrate import Migrate
 from flask import Flask
 
 current_path = os.path.dirname(os.path.abspath(__file__))
+# Config path
+# sqlite3_win = 'sqlite:///'
+# sqlite3_linux = 'sqlite:////'
 
 
 def create_app():
     app = Flask(__name__)
     path = '%s/migrations' % (current_path)
-    if app.debug == True:
+    if(platform.system == "Windowns"):
+    if (app.debug == True):
         # Env database
         # postgresql://username:password@localhost:5432/database
         # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:api@localhost:5432/postgres'
+
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/ext/db/testing/test.db' % (
             current_path)
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- src/ext/views/routes.py
+++ src/ext/views/routes.py
@@ -9,18 +9,44 @@ def init_app(app: Flask):
         return {'ola': 'mundo'}
 
     @app.route('/state/list', methods=['GET'])
-    def list_State():
+    def list_state():
         state = StateSchema(many=True)
         result = State.query.all()
         return state.jsonify(result), 200
 
     @app.route('/state/add', methods=['POST'])
-    def sreate_State():
+    def create_state():
         state = StateSchema()
-        # print(type(request.json))
-        # metodo usado com relacionametos
+        query = State.query.filter(State.cnpj == request.json['cnpj'])
+
         data = state.load(request.json, session=db.session)
-        # print(data)
-        db.session.add(data)
-        db.session.commit()
-        return state.jsonify(data), 201
+        if(query.first() == None):
+            db.session.add(data)
+            db.session.commit()
+            return state.jsonify(data), 201
+        else:
+            return jsonify({'Response': 'State already exists'}), 400
+
+    @app.route('/state/delete/<cnpj>', methods=['DELETE'])
+    def delete_state(cnpj):
+        state = State.query.filter(State.cnpj == cnpj).delete()
+
+        if(state == True):
+            db.session.commit()
+
+            return jsonify({"Delete": "State using the following CNPJ -> " + cnpj}), 200
+        else:
+            return jsonify({"Response": "State not found"}), 404
+
+    @app.route('/state/update/<cnpj>', methods=['PUT'])
+    def update_state(cnpj):
+
+        state = StateSchema()
+        data = State.query.filter(State.cnpj == cnpj)
+        if(data.first() != None):
+            data.update(request.json)
+            db.session.commit()
+            return state.jsonify(request.json), 201
+        else:
+
+            return jsonify({"Response": "State not found"}), 404
